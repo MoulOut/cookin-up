f1df39ad45fbfe06f0b946989c77c03c10529864
MoulOut <mmoulinlima@outlook.com>
2024-03-12 07:22:34 -0300

    feat: :sparkles: add: new component for tags and updates

--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -1,12 +1,14 @@
 <script lang="ts">
 import SelecionarIngredientes from './SelecionarIngredientes.vue';
+import Tag from './Tag.vue';
+
 export default {
   data() {
     return {
       ingredientes: ['Alho', 'Manteiga', 'Orégano'],
     };
   },
-  components: { SelecionarIngredientes },
+  components: { SelecionarIngredientes, Tag },
 };
 </script>
 
@@ -16,12 +18,8 @@ export default {
       <span class="subtitulo-lg sua-lista-texto">Sua lista:</span>
 
       <ul v-if="ingredientes.length" class="ingredientes-sua-lista">
-        <li
-          v-for="ingrediente in ingredientes"
-          :key="ingrediente"
-          class="ingrediente"
-        >
-          {{ ingrediente }}
+        <li v-for="ingrediente in ingredientes" :key="ingrediente">
+          <Tag :texto="ingrediente" />
         </li>
       </ul>
 
@@ -63,18 +61,6 @@ export default {
   justify-content: center;
   gap: 1rem 1.5rem;
   flex-wrap: wrap;
-}
-
-.ingrediente {
-  display: inline-block;
-  border-radius: 0.5rem;
-  min-width: 4.25rem;
-  padding: 0.5rem;
-  text-align: center;
-  transition: 0.2s;
-  color: var(--creme, #fffaf3);
-  background: var(--coral, #f0633c);
-  font-weight: 700;
 }
 
 .lista-vazia {
